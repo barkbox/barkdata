e70b83e908cdfe03e6c4c36ce96201ad804dfc61
Cris Kim <cris@criskim.com>
2016-03-10 11:22:08 -0500

    updaet aws integration

--- lib/barkdata/s3.rb
+++ lib/barkdata/s3.rb
@@ -12,13 +12,14 @@ module Barkdata
       s3_key = [ prefix, File.basename(filepath) ].join('/')
       Rails.logger.info "Barkdata::S3.upload filepath=#{filepath.inspect} to " +
                         "s3_key=#{s3_key.inspect}"
-      s3 = AWS::S3.new({
+      s3 = Aws::S3::Client.new({
         access_key_id: ENV['AWS_ACCESS_KEY_ID'],
         secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
       })
-      bucket = s3.buckets[@bucket_name]
       file = File.open(filepath, 'rb')
-      bucket.objects[s3_key].write(file)
+      s3.put_object(bucket: @bucket_name,
+                       key: s3_key,
+                      body: file)
       file.close
       Rails.logger.info "Barkdata::S3.upload Finished uploading " +
                         "filepath=#{filepath.inspect} to " +
